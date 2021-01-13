be350c6d37bd71d023288913aef96dc281db3ef6
Tristan Elliott <>
2021-01-13 12:46:51 -0400

    creating the redux store

--- redux-app/src/index.js
+++ redux-app/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
 
 import App from './Components/App'
+import tasks from './reducers'
+
+const store = createStore(tasks)
+
 
 
 ReactDOM.render(<App />,document.getElementById('root'))
